b2f67969bcbb1c4ddbeafb8603ce6ea9aaca1a7d
Marcel <MTRNord@users.noreply.github.com>
2016-09-16 22:35:45 +0200

    Update view-header.js

--- view-header.js
+++ view-header.js
@@ -20,7 +20,7 @@ module.exports = function () {
 }
 
 function banners () {
-  if (config.web.enabled && config.web.user === 'admin' && config.web.pass === 'secret') {
+  if (config.web.enabled && process.env.user === 'admin' && process.env.pass === 'secret') {
     return h('.banner', [
       h('p', [
         'You have web enabled but have not changed the default user and password setup; this means anyone can come along and view your crash reports. ',
